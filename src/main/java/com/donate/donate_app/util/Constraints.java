485c1d08a2a13d773ef6b476d16cddd1caa1910f
JGabriel-SL <gabrieldesouzalima44@gmail.com>
2024-10-13 15:01:27 -0300

    removing credentials

--- src/main/java/com/donate/donate_app/util/Constraints.java
+++ src/main/java/com/donate/donate_app/util/Constraints.java
@@ -1,5 +1,5 @@
 package com.donate.donate_app.util;
 
 public class Constraints {
-    public static final String FIREBASE_AUTH_PATH = "/Users/joaovitor/Documents/programação/vaquinha-backend/src/main/java/com/donate/donate_app/donate-app-33f84-firebase-adminsdk-p2cqc-f3b3dab906.json";
+    public static final String FIREBASE_AUTH_PATH = "D:\\projetos\\vaquinha-backend\\src\\main\\java\\com\\donate\\donate_app\\crowdfunding-f4d9e-dbf2cd0e662e.json";
 }
